4460de9a9f1c53de39eff8d078997589990c0d0f
thisdotrob <this.rob@protonmail.com>
2023-11-08 17:43:46 +0000

    Timer logic v2

--- src/cli.rs
+++ src/cli.rs
@@ -1,45 +1,64 @@
 use crate::timer::TimerArgs;
-use indicatif::MultiProgress;
+use std::io::{Write, stdout};
 use std::io;
-use std::sync::{Arc, Mutex, mpsc};
+use crossterm::{QueueableCommand, cursor};
+
+use std::sync::{Arc, mpsc};
 
 fn clear_terminal() {
     print!("{}[2J", 27 as char);
 }
 
-pub fn start(progress_bars: Arc<Mutex<MultiProgress>>, timer_register_tx: mpsc::Sender<TimerArgs>) {
+pub fn start(timer_register_tx: mpsc::Sender<TimerArgs>) {
+    let mut row = 0;
+
     loop {
-        clear_terminal();
+         clear_terminal();
 
         println!("Press return to start adding a new timer");
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let progress_bars = progress_bars.lock().unwrap();
+        clear_terminal();
 
-        progress_bars.suspend(|| {
-            clear_terminal();
+        println!("Provide stdin with a string in the following format to register a new timer:");
+        println!("name repetitions interval(ms)");
+        println!("e.g.: \"timer0 100 50\"");
 
-            println!("Provide stdin with a string in the following format to register a new timer:");
-            println!("name repetitions interval(ms)");
-            println!("e.g.: \"timer0 100 50\"");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
+        let mut split_input = input.split_whitespace();
 
-            let mut split_input = input.split_whitespace();
+        let name = split_input.next().unwrap();
+        let repetitions = split_input.next().unwrap();
+        let repetitions: isize = repetitions.parse().expect("Failed to parse numeric string");
+        let interval = split_input.next().unwrap();
+        let interval: i64 = interval.parse().expect("Failed to parse numeric string");
 
-            let name = split_input.next().unwrap();
-            let repetitions = split_input.next().unwrap();
-            let repetitions: isize = repetitions.parse().expect("Failed to parse numeric string");
-            let interval = split_input.next().unwrap();
-            let interval: i64 = interval.parse().expect("Failed to parse numeric string");
+        let state = format!("Some state given to the {} timer's callback", name);
 
-            let timer_args = TimerArgs {
-                name: String::from(name), repetitions, interval
-            };
+        let callback: Arc<dyn Fn() + Send + Sync> = Arc::new(move || {
+            let mut stdout = stdout();
 
-            timer_register_tx.send(timer_args).unwrap();
-        })
+            let ticks = crate::timer::current_ticks();
+
+            stdout.queue(cursor::SavePosition).expect("Error saving position");
+            stdout.queue(cursor::MoveTo(0, row)).expect("Error moving cursor");
+            print!("{} - {}", state, ticks);
+            stdout.queue(cursor::RestorePosition).expect("Error restoring position");
+            stdout.flush().expect("Error flushing");
+        });
+
+        row += 1;
+
+        let timer_args = TimerArgs {
+            name: String::from(name),
+            repetitions,
+            interval,
+            callback,
+        };
+
+        timer_register_tx.send(timer_args).unwrap();
     }
 }
--- src/timer.rs
+++ src/timer.rs
@@ -1,32 +1,33 @@
 use chrono::prelude::*;
 use std::sync::{Arc, Mutex, mpsc};
-use indicatif::MultiProgress;
 
 mod registration_thread;
 mod prioritisation_thread;
 mod execution_thread;
 
-#[derive(Debug)]
+type CallBack = Arc<dyn Fn() + Send + Sync>;
+
 pub struct Timer {
     name: String,
     repetitions: isize,
     interval: i64,
     next: i64, // TODO rename to `next_tick`?
+    callback: CallBack,
 }
 
-#[derive(Debug)]
 pub struct TimerArgs {
     pub name: String,
     pub repetitions: isize,
     pub interval: i64,
+    pub callback: CallBack,
 }
 
-fn current_ticks() -> i64 {
+pub fn current_ticks() -> i64 {
     let utc_now = Utc::now();
     utc_now.timestamp_millis()
 }
 
-pub fn start(progress_bars: Arc<Mutex<MultiProgress>>) -> mpsc::Sender<TimerArgs> {
+pub fn start() -> mpsc::Sender<TimerArgs> {
     let (register_tx, register_rx) = mpsc::channel::<TimerArgs>();
     let (execute_tx, execute_rx) = mpsc::channel::<Timer>();
 
@@ -35,7 +36,7 @@ pub fn start(progress_bars: Arc<Mutex<MultiProgress>>) -> mpsc::Sender<TimerArgs
 
     registration_thread::spawn(register_rx, Arc::clone(&new_timers));
     prioritisation_thread::spawn(execute_tx, new_timers);
-    execution_thread::spawn(execute_rx, progress_bars);
+    execution_thread::spawn(execute_rx);
 
     return register_tx
 }
