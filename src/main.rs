66cd3273062ced3afbfc94fbfef6075a1fddee38
thisdotrob <this.rob@protonmail.com>
2023-11-08 16:30:47 +0000

    Refactor v1 timer logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use indicatif::MultiProgress;
+use std::sync::{Arc, Mutex};
+
 mod timer;
 mod cli;
 
 fn main() {
-    let (progress_bars, timer_register_tx) = timer::start();
+    let progress_bars = Arc::new(Mutex::new(MultiProgress::new()));
+
+    let timer_register_tx = timer::start(Arc::clone(&progress_bars));
+
     cli::start(progress_bars, timer_register_tx);
 }
---
+++ src/timer/prioritisation_thread.rs
@@ -0,0 +1,44 @@
+use std::sync::{Arc, Mutex, mpsc};
+use std::thread;
+use std::time::Duration;
+use super::{Timer, current_ticks};
+
+pub fn spawn(execute_tx: mpsc::Sender<Timer>, new_timers: Arc<Mutex<Vec<Timer>>>) {
+    let _timer_prioritiser_thread = thread::spawn(move || {
+        let mut timers = vec![];
+
+        loop {
+            thread::sleep(Duration::from_millis(1));
+            {
+                let mut new_timers = new_timers.lock().unwrap();
+
+                while let Some(timer) = new_timers.pop() {
+                    timers.push(timer);
+                }
+            }
+
+            let mut not_due = vec![];
+
+            let now = current_ticks();
+
+            for timer in timers {
+                if timer.next < now {
+                    if timer.repetitions > 1 {
+                        let next_repetition = Timer {
+                            name: String::from(&timer.name),
+                            repetitions: timer.repetitions - 1,
+                            interval: timer.interval,
+                            next: timer.next + timer.interval,
+                        };
+                        not_due.push(next_repetition);
+                    }
+                    execute_tx.send(timer).unwrap();
+                } else {
+                    not_due.push(timer);
+                }
+            }
+
+            timers = not_due;
+        }
+    });
+}
